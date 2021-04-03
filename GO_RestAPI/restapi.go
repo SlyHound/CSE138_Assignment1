0baa24299c030ab2d760d594ec083c4300c4bcde
Oleksiy <oomelche@ucsc.edu>
2021-04-03 14:48:14 -0700

    functionality for /echo

--- GO_RestAPI/restapi.go
+++ GO_RestAPI/restapi.go
@@ -26,5 +26,18 @@ func main() {
 		c.AbortWithStatus(http.StatusMethodNotAllowed) // 405 status code error
 	})
 
-	r.Run() // default is port number 8080
+	r.GET("/echo", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message" : "Get Message Received"}) // 200 status code
+	})
+
+	r.POST("/echo", func(c *gin.Context)  {
+		m := c.Query("msg")
+		if m != "" {
+			c.JSON(http.StatusOK, gin.H{"message" : m}) // 200 status code
+		} else {
+			c.AbortWithStatus(http.StatusBadRequest) // 400 status code error
+		}
+	})
+
+	r.Run(":8085") // default port number is 8080
 }
